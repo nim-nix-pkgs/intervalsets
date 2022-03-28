1d99f9e83edc613d8200f36682a8adbe9f76fadb
hugosenari <hugosenari@gmail.com>
2022-03-27 21:50:50 -0300

    chore: re index intervalsets

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-intervalsets-master.ref   = "refs/heads/master";
   inputs.src-intervalsets-master.owner = "autumngray";
   inputs.src-intervalsets-master.repo  = "intervalsets";
-  inputs.src-intervalsets-master.dir   = "";
   inputs.src-intervalsets-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
